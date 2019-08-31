ba71d3c7357a1eb81c3075cf3313014e432c20ca
Chasmer02 <chasmer.plays@gmail.com>
2019-08-31 21:02:05 +0200

    Initial Commit

---
+++ Delivr Test Game/Assets/Scripts/MeshColliderController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshColliderController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[filters.Length];
+
+        Debug.Log(combine.Length);
+
+        for(int i = 0; i < filters.Length; i++)
+        {
+            combine[i].mesh = filters[i].sharedMesh;
+            combine[i].transform = filters[i].transform.localToWorldMatrix;
+            filters[i].gameObject.SetActive(false);
+            
+        }
+        GetComponent<MeshFilter>().sharedMesh = new Mesh();
+        GetComponent<MeshFilter>().sharedMesh.CombineMeshes(combine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("HIT!");
+    }
+}
---
+++ Delivr Test Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public int speed;
+
+    public int angleX;
+    public int angleY;
+    public int angleZ;
+
+    public float smooth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddRelativeForce(new Vector3(0, 0, speed));
+
+        float tiltX = Input.GetAxis("Vertical") * angleX;
+        float tiltY = Input.GetAxis("Horizontal") * angleY;
+
+        Debug.Log(tiltX + ", " + tiltY);
+
+        Quaternion target = Quaternion.Euler(new Vector3(transform.rotation.x + tiltX,transform.rotation.y + tiltY, 0));
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+}
